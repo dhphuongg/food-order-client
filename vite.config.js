3e4b1c88b167425b6af8f32332cc4e60205b913a
phuonghight <hoangphuong270703@gmail.com>
2024-02-01 15:17:19 +0700

    feat: add prettier as formater code

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@use '@/styles/_variables.scss' as *;`,
+        additionalData: `@use '@/styles/_variables.scss' as *; @use '@/styles/_mixins.scss' as *;`,
       },
     },
   },
